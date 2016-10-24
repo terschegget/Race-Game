6d285b6edcc047e4a40749eaea2fe964314ba113
terschegget <twanterschegget@hotmail.nl>
2016-10-24 14:43:27 +0200

    versie 0.2
    Begin van de bestuuring

---
+++ Race Game/Race Game/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Race_Game
+{
+    class Car
+    {
+        private Point position;
+        private float rotation;
+        private double speed;
+        private Keys leftKey, rightKey, upKey, downKey;
+        private Image image;
+
+        public Car(int positionX, int positionY, float rotation, double speed, Keys leftKey, Keys rightKey, Keys upKey, Keys downKey, Image image)
+        {
+            position.X = positionX;
+            position.Y = positionY;
+            this.rotation = rotation;
+            this.leftKey = leftKey;
+            this.rightKey = rightKey;
+            this.upKey = upKey;
+            this.downKey = downKey;
+            this.image = image;
+        }
+
+        public Car()
+        {
+        }
+    }
+}
--- Race Game/Race Game/Form1.cs
+++ Race Game/Race Game/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,21 @@ using System.Windows.Forms;
 
 namespace Race_Game
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
+    public partial class FormRaceGame : Form
+    {   
+        Bitmap Backbuffer;
+
+        public FormRaceGame()
         {
             InitializeComponent();
+
+            Car car1 = new Car(30, 30, 0, 0, Keys.Left, Keys.Right, Keys.Up, Keys.Down, new Bitmap(Path.Combine(Environment.CurrentDirectory, "pijl.png")));
+
+            this.SetStyle(
+            ControlStyles.UserPaint |
+            ControlStyles.AllPaintingInWmPaint |
+            ControlStyles.DoubleBuffer, true);
+
         }
     }
 }
