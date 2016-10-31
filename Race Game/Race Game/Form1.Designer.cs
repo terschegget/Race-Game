16210bd1aa5cb9f3f7afb5cf562d9cffc45a34d7
Burningwaflz <muismatje3@gmail.com>
2016-11-01 00:05:07 +0100

    Versie 0.9
    Baan is bijna af, moet alleen nog achtergrond bij. Verder missen we
    collisions, checkpoints en de pitstop doet het nog niet

--- Race Game/Race Game/Form1.Designer.cs
+++ Race Game/Race Game/Form1.Designer.cs
@@ -42,6 +42,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
             this.ClientSize = new System.Drawing.Size(1008, 729);
             this.MaximumSize = new System.Drawing.Size(1024, 768);
             this.Name = "FormRaceGame";
--- Race Game/Race Game/Form1.cs
+++ Race Game/Race Game/Form1.cs
@@ -16,11 +16,33 @@ namespace Race_Game
         List<Car> cars = new List<Car>();
         private object b;
 
+        //Initalizatie van alle stukken van de baan
+        Bitmap trackPitsEnter = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/pitstopBocht1Rechts.png"));
+        Bitmap trackPitsExit = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/pitstopBocht1Links2.png"));
+        Bitmap trackPitstop = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/pitstop.png"));
         Bitmap trackFinish = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/start.png"));
-        Bitmap trackStraight = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanrechtgoeie.png"));
-        Bitmap trackStraight2 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanrechtgoeie.png"));
+        Bitmap trackPitsCornerRight = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/pitstopBocht2Rechts.png"));
+        Bitmap trackPitsCornerLeft = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/pitstopBocht2Links.png"));
+        Bitmap trackStraight1 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtBreed.png"));
+        Bitmap trackStraight2 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtBreed.png"));
+        Bitmap trackStraight3 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtBreed.png"));
+        Bitmap trackCorner1 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht2v4.png"));
+        Bitmap trackCorner2 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht1v3.png"));
+        Bitmap trackCorner3 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht2v1.png"));
+        Bitmap trackCorner4 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht2v3.png"));
+        Bitmap trackCorner5 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht1v1.png"));
+        Bitmap trackCorner6 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht1v2.png"));
+        Bitmap trackCorner7 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht1v4.png"));
+        Bitmap trackCorner8 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht1v3.png"));
+        Bitmap trackCorner9 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht1v4.png"));
+        Bitmap trackCorner10 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/bocht1v2.png"));
+        Bitmap trackStraightUp1 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtHoog.png"));
+        Bitmap trackStraightUp2 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtHoog.png"));
+        Bitmap trackStraightUp3 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtHoog.png"));
+        Bitmap trackStraightUp4 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtHoog.png"));
+        Bitmap trackStraightUp5 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtHoog.png"));
+        Bitmap trackStraightUp6 = new Bitmap(Path.Combine(Environment.CurrentDirectory, "resources/sprites/baanRechtHoog.png"));
 
-        
         public FormRaceGame()
         {
             InitializeComponent();
@@ -74,9 +96,35 @@ namespace Race_Game
 
         void Form1_PaintTrack(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawImage(trackFinish, ClientSize.Width/2 - 64, ClientSize.Height - 352);
-            e.Graphics.DrawImage(trackStraight, ClientSize.Width / 2 - 188, ClientSize.Height - 352);
-            e.Graphics.DrawImage(trackStraight2, ClientSize.Width / 2 - 256, ClientSize.Height - 352);
+            //Het grafisch tekenen van elk stuk van de baan
+            e.Graphics.DrawImage(trackCorner2, ClientSize.Width / 2 + 320, ClientSize.Height - 224);
+            e.Graphics.DrawImage(trackStraight2, ClientSize.Width / 2 + 192, ClientSize.Height - 224);
+            e.Graphics.DrawImage(trackPitsEnter, ClientSize.Width / 2 + 64, ClientSize.Height - 224);
+            e.Graphics.DrawImage(trackPitsCornerRight, ClientSize.Width / 2 + 64, ClientSize.Height - 352);
+            e.Graphics.DrawImage(trackPitsCornerLeft, ClientSize.Width / 2 - 192, ClientSize.Height - 352);
+            e.Graphics.DrawImage(trackPitstop, ClientSize.Width / 2 - 64, ClientSize.Height - 352);
+            e.Graphics.DrawImage(trackFinish, ClientSize.Width/2 - 64, ClientSize.Height - 224);
+            e.Graphics.DrawImage(trackPitsExit, ClientSize.Width / 2 - 192, ClientSize.Height - 224);
+            e.Graphics.DrawImage(trackStraight1, ClientSize.Width / 2 - 320, ClientSize.Height - 224);
+            e.Graphics.DrawImage(trackCorner1, ClientSize.Width / 2 - 448, ClientSize.Height - 224);
+            e.Graphics.DrawImage(trackStraightUp1, ClientSize.Width / 2 - 448, ClientSize.Height - 352);
+            e.Graphics.DrawImage(trackCorner3, ClientSize.Width / 2 - 448, ClientSize.Height - 480);
+            e.Graphics.DrawImage(trackStraight3, ClientSize.Width / 2 - 320, ClientSize.Height - 480);
+            e.Graphics.DrawImage(trackCorner4, ClientSize.Width / 2 - 192, ClientSize.Height - 480);
+            e.Graphics.DrawImage(trackStraightUp2, ClientSize.Width / 2 - 192, ClientSize.Height - 608);
+            e.Graphics.DrawImage(trackCorner5, ClientSize.Width / 2 - 192, ClientSize.Height - 736);
+            e.Graphics.DrawImage(trackCorner6, ClientSize.Width / 2 -64, ClientSize.Height - 736);
+            e.Graphics.DrawImage(trackStraightUp3, ClientSize.Width / 2 - 64, ClientSize.Height - 608);
+            e.Graphics.DrawImage(trackCorner7, ClientSize.Width / 2 - 64, ClientSize.Height - 480);
+            e.Graphics.DrawImage(trackCorner8, ClientSize.Width / 2 + 64 , ClientSize.Height - 480);
+            e.Graphics.DrawImage(trackStraightUp4, ClientSize.Width / 2 + 64, ClientSize.Height - 608);
+            e.Graphics.DrawImage(trackCorner5, ClientSize.Width / 2 + 64, ClientSize.Height - 736);
+            e.Graphics.DrawImage(trackCorner6, ClientSize.Width / 2 + 192, ClientSize.Height - 736);
+            e.Graphics.DrawImage(trackStraightUp5, ClientSize.Width / 2 + 192, ClientSize.Height - 608);
+            e.Graphics.DrawImage(trackStraightUp6, ClientSize.Width / 2 + 192, ClientSize.Height - 480);
+            e.Graphics.DrawImage(trackCorner9, ClientSize.Width / 2 + 192, ClientSize.Height - 352);
+            e.Graphics.DrawImage(trackCorner10, ClientSize.Width / 2 + 320, ClientSize.Height - 352);
+
         }
         
         void Form1_CreateBackBuffer(object sender, EventArgs e) {
