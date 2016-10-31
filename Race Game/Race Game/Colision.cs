60dfdaa36785f028ec3fb53d375a39b4e863582e
terschegget <twanterschegget@hotmail.nl>
2016-10-31 14:28:50 +0100

    Versie 0.7.2

---
+++ Race Game/Race Game/Colision.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Race_Game
+{
+    class Colision
+    {
+        public void makeCarBounds(int width, int height, double rotation)
+        {
+            double angleRotation = rotation * (180.0 / Math.PI);
+
+            Point pointA;
+            pointA.X = (int)Math.Sin(angleRotation) * 30.9;
+        }
+    }
+}
